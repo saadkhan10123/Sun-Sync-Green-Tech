7398781220e7d434e7b69868820d7af3240ed6fd
Muhammad Saad Khan <saadkhan10123@gmail.com>
2024-03-30 15:59:46 +0500

    test 2

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,3 +1,15 @@
 <main>
     <h1>Hello World!</h1>
 </main>
+
+<style>
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    h1 {
+        font-size: 2rem;
+    }
+</style>
